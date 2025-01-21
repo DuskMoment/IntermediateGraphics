6a047360a6c5f78788f8d70bb4f9c7cc502bc57c
DuskMoment <william.mansfield@mymail.champlain.edu>
2025-01-20 20:49:04 -0500

    addition of assignment 1

---
+++ assignments/assignment1/assets/lit.vert
@@ -0,0 +1,44 @@
+#version 450
+
+//vertex attributes
+layout(location = 0) in vec3 in_Pos;
+layout(location = 1) in vec3 in_Normal;
+layout(location = 2) in vec2 in_TexCoords;
+layout(location = 3) in vec3 in_Tangent;
+
+uniform mat4 _Model;
+
+uniform mat4 _VeiwProjection;
+
+
+out Surface{
+	vec3 WorldPos;
+	vec3 WorldNormal;
+	vec2 TexCoord;
+	mat3 TBN;
+}vs_out;
+
+
+void main()
+{
+	//model to world space converstions
+	vs_out.WorldPos = vec3(_Model * vec4(in_Pos,1.0));
+	vs_out.WorldNormal = transpose(inverse(mat3(_Model))) * in_Normal;
+
+	vec3 T = normalize(vec3(_Model * vec4(in_Tangent, 0.0)));
+	vec3 N = normalize(vec3(_Model * vec4(in_Normal, 0.0)));
+
+	//re-orthogonalize T with respect to N 
+	T = normalize(T - dot(T,N) * N);
+
+	//vec3 B = normalize(vec3(_Model * vec4(in_biTangnet, 0.0)));
+	vec3 B = cross(N,T);
+
+	vs_out.TBN = transpose(mat3(T,B,N));
+	//vs_out.TBN = mat3(T,B,N);
+
+	//texutre
+	vs_out.TexCoord = in_TexCoords;
+
+	gl_Position = _VeiwProjection * _Model * vec4(in_Pos, 1.0);
+}
---
+++ assignments/assignment1/main.cpp
@@ -0,0 +1,246 @@
+#include <stdio.h>
+#include <math.h>
+
+#include <ew/external/glad.h>
+
+#include <GLFW/glfw3.h>
+#include <imgui.h>
+#include <imgui_impl_glfw.h>
+#include <imgui_impl_opengl3.h>
+
+#include <ew/shader.h>
+#include <ew/model.h>
+#include <ew/camera.h>
+#include <ew/cameraController.h>
+#include <ew/transform.h>
+#include <ew/texture.h>
+
+void framebufferSizeCallback(GLFWwindow* window, int width, int height);
+GLFWwindow* initWindow(const char* title, int width, int height);
+void drawUI();
+
+//Global state
+int screenWidth = 1080;
+int screenHeight = 720;
+float prevFrameTime;
+float deltaTime;
+
+//camera
+ew::Camera camera;
+ew::CameraController controller;
+
+//transfrom
+ew::Transform modelTrans;
+
+//texutres
+GLuint brickTexture;
+GLuint normalMapping;
+
+
+
+struct Material 
+{
+	float Ka = 1.0;
+	float Kd = 0.5;
+	float Ks = 0.5;
+	float Shininess = 128;
+}material;
+
+struct Framebuffer
+{
+	GLuint fbo;
+	GLuint color0;
+	GLuint color1;
+	GLuint color2;
+	GLuint depth;
+
+};
+
+void renderMonekey(ew::Shader& shader, ew::Model& model, GLFWwindow* window)
+{
+	
+	//pipeline definition
+	glEnable(GL_CULL_FACE);
+	glCullFace(GL_BACK);
+
+	glEnable(GL_DEPTH_TEST);
+
+	//create a gfx pass
+	glClearColor(0.6f, 0.8f, 0.92f, 1.0f);
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+	//texture
+	glActiveTexture(GL_TEXTURE0);
+	glBindTexture(GL_TEXTURE_2D, brickTexture);
+
+	glActiveTexture(GL_TEXTURE1);
+	glBindTexture(GL_TEXTURE_2D, normalMapping);
+
+
+	shader.use();
+	
+	//camera uniforms
+	shader.setMat4("_VeiwProjection", camera.projectionMatrix() * camera.viewMatrix());
+	shader.setVec3("_EyePos", camera.position);
+
+	//model uniforms
+	modelTrans.rotation = glm::rotate(modelTrans.rotation, deltaTime, glm::vec3(0.0, 1.0, 0.0));
+	shader.setMat4("_Model", modelTrans.modelMatrix());
+
+	//material
+	shader.setFloat("_Material.Ka", material.Ka);
+	shader.setFloat("_Material.Kd", material.Kd);
+	shader.setFloat("_Material.Ks", material.Ks);
+	shader.setFloat("_Material.Shininess", material.Shininess);
+
+	//textures
+	shader.setInt("_MainTex", 0);
+	shader.setInt("_NormalMap", 1);
+
+	model.draw();
+
+	controller.move(window, &camera, deltaTime);
+
+}
+
+void resetCamera(ew::Camera* camera, ew::CameraController* controller)
+{
+	camera->position = glm::vec3(0.0, 0.0, 5.0f);
+	camera->target = glm::vec3(0);
+	controller->yaw = controller->pitch = 0;
+
+}
+
+int main() {
+
+	GLFWwindow* window = initWindow("Assignment 0", screenWidth, screenHeight);
+	glfwSetFramebufferSizeCallback(window, framebufferSizeCallback);
+
+	//chache 
+	
+	ew::Shader shader = ew::Shader("assets/lit.vert", "assets/lit.frag");
+	ew::Model model = ew::Model("assets/Suzanne.fbx");
+
+	Framebuffer buffer;
+
+	//init camera
+	camera.position = glm::vec3(0.0f, 0.0f, 5.0f);
+	camera.target = glm::vec3(0.0f, 0.0f, 0.0f);
+	camera.aspectRatio = (float)screenWidth / screenHeight;
+	camera.fov = 60.0f;
+
+	//texture
+	brickTexture = ew::loadTexture("assets/bricks/Bricks075A_1K-JPG_Color.jpg");
+	normalMapping = ew::loadTexture("assets/bricks/Bricks075A_1K-JPG_NormalDX.jpg");
+	//brickTexture = ew::loadTexture("assets/brick_color.jpg");
+	
+	glGenFramebuffers(1, &buffer.fbo);
+	glBindFramebuffer(GL_FRAMEBUFFER, buffer.fbo);
+
+	glGenTextures(1, &buffer.color0);
+	glBindTexture(GL_TEXTURE_2D, buffer.color0);
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, 800, 600, 0, GL_RGB, GL_UNSIGNED_BYTE, nullptr);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+	glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, buffer.color0, 0);
+
+	if (glCheckFramebufferStatus(GL_FRAMEBUFFER) != GL_FRAMEBUFFER_COMPLETE)
+	{
+		printf("frame buffer is not complete!");
+		return 0;
+
+	}
+
+
+	while (!glfwWindowShouldClose(window)) {
+		glfwPollEvents();
+
+		float time = (float)glfwGetTime();
+		deltaTime = time - prevFrameTime;
+		prevFrameTime = time;
+
+		//RENDER
+		glClearColor(0.6f,0.8f,0.92f,1.0f);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+		renderMonekey(shader, model, window);
+
+		drawUI();
+		
+
+		glfwSwapBuffers(window);
+	}
+	printf("Shutting down...");
+
+}
+
+void drawUI() {
+	ImGui_ImplGlfw_NewFrame();
+	ImGui_ImplOpenGL3_NewFrame();
+	ImGui::NewFrame();
+
+	ImGui::Begin("Settings");
+	ImGui::Text("Add Controls Here!");
+	
+	if (ImGui::Button("Reset Camera"))
+	{
+		resetCamera(&camera, &controller);
+	}
+	if (ImGui::CollapsingHeader("Material"))
+	{
+		ImGui::SliderFloat("AmbientK", &material.Ka, 0.0f, 1.0f);
+		ImGui::SliderFloat("DiffuseK", &material.Kd, 0.0f, 1.0f);
+		ImGui::SliderFloat("SepcularK", &material.Ks, 0.0f, 1.0f);
+		ImGui::SliderFloat("Shininess", &material.Shininess, 2.0f, 1024.0f);
+
+	}
+	
+	ImGui::End();
+
+	ImGui::Render();
+	ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+}
+
+void framebufferSizeCallback(GLFWwindow* window, int width, int height)
+{
+	glViewport(0, 0, width, height);
+	screenWidth = width;
+	screenHeight = height;
+}
+
+/// <summary>
+/// Initializes GLFW, GLAD, and IMGUI
+/// </summary>
+/// <param name="title">Window title</param>
+/// <param name="width">Window width</param>
+/// <param name="height">Window height</param>
+/// <returns>Returns window handle on success or null on fail</returns>
+GLFWwindow* initWindow(const char* title, int width, int height) {
+	printf("Initializing...");
+	if (!glfwInit()) {
+		printf("GLFW failed to init!");
+		return nullptr;
+	}
+
+	GLFWwindow* window = glfwCreateWindow(width, height, title, NULL, NULL);
+	if (window == NULL) {
+		printf("GLFW failed to create window");
+		return nullptr;
+	}
+	glfwMakeContextCurrent(window);
+
+	if (!gladLoadGL(glfwGetProcAddress)) {
+		printf("GLAD Failed to load GL headers");
+		return nullptr;
+	}
+
+	//Initialize ImGUI
+	IMGUI_CHECKVERSION();
+	ImGui::CreateContext();
+	ImGui_ImplGlfw_InitForOpenGL(window, true);
+	ImGui_ImplOpenGL3_Init();
+
+	return window;
+}
+
